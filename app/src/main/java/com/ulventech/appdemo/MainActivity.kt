f6e342ffee894b23cf736893b7b465e2e1ae6844
L007 <antoeldi@gmail.com>
2021-11-18 09:16:23 +0700

    Initial commit

---
+++ app/src/main/java/com/ulventech/appdemo/MainActivity.kt
@@ -0,0 +1,146 @@
+package com.ulventech.appdemo
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var callbackManager: CallbackManager
+
+
+
+
+    @SuppressLint("ResourceAsColor")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val defaults = mapOf(
+            "font_size" to 14,
+            "font_color" to "#ff0000"
+        )
+
+
+        val remoteConfig = FirebaseRemoteConfig.getInstance()
+        remoteConfig.setDefaultsAsync(defaults)
+
+
+
+        remoteConfig.fetchAndActivate().addOnCompleteListener {
+
+            if(it.isSuccessful){
+                var fontSize = remoteConfig.getDouble("font_size")
+                var fontColor = remoteConfig.getString("font_color")
+                Log.d("remote_config","fetched complete, ${fontSize}, ${fontColor}")
+
+                txtRemoteConfig.textSize = fontSize.toFloat()
+                txtRemoteConfig.setTextColor((Color.parseColor(fontColor)))
+            }
+            else{
+                Log.d("remote_config","fail")
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
+
+
+        auth = Firebase.auth
+
+        // Initialize Facebook Login button
+        callbackManager = CallbackManager.Factory.create()
+
+        login_button.setReadPermissions("email", "public_profile")
+        login_button.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d("Success", "facebook:onSuccess:$loginResult")
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "facebook:onError", error)
+            }
+        })
+        // ...
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+    // [START auth_with_facebook]
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        Log.d(TAG,"${user?.email}, ${user?.displayName}, ${user?.photoUrl}")
+    }
+
+    companion object {
+        private const val TAG = "FacebookLogin"
+    }
+
+}
